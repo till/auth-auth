74445df1f9ef0c8637eac432a7c8dc33d202a345
Morgan Roderick <morgan@roderick.dk>
2025-08-29 12:36:19 +0200

    chore: add colour to success and failure messages

--- src/app/components/common.js
+++ src/app/components/common.js
@@ -5,6 +5,14 @@ export const FormSection = ({ children }) => html` <div>${children}</div> `;
 
 // Message display component
 export const Message = ({ error, success }) => html`
-  ${error ? html`<div>${decodeURIComponent(error)}</div>` : ""}
-  ${success ? html`<div>${decodeURIComponent(success)}</div>` : ""}
+  ${error
+    ? html`<div class="pico-background-red-50">
+        ${decodeURIComponent(error)}
+      </div>`
+    : ""}
+  ${success
+    ? html`<div class="pico-background-green-50">
+        ${decodeURIComponent(success)}
+      </div>`
+    : ""}
 `;
